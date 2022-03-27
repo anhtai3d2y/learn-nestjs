335d382112796d4f0829f6a7e7d78f8c18312b90
Tai <anhtai3d2y@gmail.com>
2022-03-27 12:18:35 +0700

    dto

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,39 +1,40 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  Param,
-  Patch,
-  Post,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpCode,
+    Param,
+    Patch,
+    Post,
 } from '@nestjs/common';
+import { CreateEventDto } from './create-event-dto';
 
 @Controller('/events')
 export class EventsController {
-  @Get()
-  findAll() {
-    return [
-      { id: 1, name: 'Cuong ngu' },
-      { id: 2, name: 'Cuong ngu' },
-      { id: 3, name: 'Cuong ngu' },
-      { id: 4, name: 'Cuong ngu' },
-      { id: 5, name: 'Cuong ngu' },
-    ];
-  }
-  @Get(':id')
-  findOne(@Param('id') id) {
-    return id;
-  }
-  @Post()
-  create(@Body() input) {
-    return input;
-  }
-  @Patch(':id')
-  update(@Param('id') id, @Body() input) {
-    return input;
-  }
-  @Delete(':id')
-  @HttpCode(204)
-  remove(@Param('id') id) {}
+    @Get()
+    findAll() {
+        return [
+            { id: 1, name: 'Cuong ngu' },
+            { id: 2, name: 'Cuong ngu' },
+            { id: 3, name: 'Cuong ngu' },
+            { id: 4, name: 'Cuong ngu' },
+            { id: 5, name: 'Cuong ngu' },
+        ];
+    }
+    @Get(':id')
+    findOne(@Param('id') id) {
+        return id;
+    }
+    @Post()
+    create(@Body() input: CreateEventDto) {
+        return input;
+    }
+    @Patch(':id')
+    update(@Param('id') id, @Body() input) {
+        return input;
+    }
+    @Delete(':id')
+    @HttpCode(204)
+    remove(@Param('id') id) {}
 }
