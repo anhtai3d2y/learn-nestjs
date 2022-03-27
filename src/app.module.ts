2a460adba44963cb096c30a621eaaadafb484769
Tai <anhtai3d2y@gmail.com>
2022-03-27 19:34:32 +0700

    custom module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Event } from './event.entity';
+import { Event } from './events/event.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EventsController } from './events.controller';
+import { EventsModule } from './events/events.module';
 
 @Module({
     imports: [
@@ -17,9 +17,9 @@ import { EventsController } from './events.controller';
             entities: [Event],
             synchronize: true,
         }),
-        TypeOrmModule.forFeature([Event]),
+        EventsModule,
     ],
-    controllers: [AppController, EventsController],
+    controllers: [AppController],
     providers: [AppService],
 })
 export class AppModule {}
