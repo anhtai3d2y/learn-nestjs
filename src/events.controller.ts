04be87757047023fef6b9a6909fb965b550c7ed6
Tai <anhtai3d2y@gmail.com>
2022-03-27 12:15:13 +0700

    add request body

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,17 +1,39 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('/events')
 export class EventsController {
   @Get()
-  findAll() {}
+  findAll() {
+    return [
+      { id: 1, name: 'Cuong ngu' },
+      { id: 2, name: 'Cuong ngu' },
+      { id: 3, name: 'Cuong ngu' },
+      { id: 4, name: 'Cuong ngu' },
+      { id: 5, name: 'Cuong ngu' },
+    ];
+  }
   @Get(':id')
   findOne(@Param('id') id) {
     return id;
   }
   @Post()
-  create() {}
+  create(@Body() input) {
+    return input;
+  }
   @Patch(':id')
-  update(@Param('id') id) {}
+  update(@Param('id') id, @Body() input) {
+    return input;
+  }
   @Delete(':id')
+  @HttpCode(204)
   remove(@Param('id') id) {}
 }
